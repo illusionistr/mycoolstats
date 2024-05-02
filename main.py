5db0885719dd35bea966ee4460a839703b205bcc
Bonnaire Fils Emmanuel <149618934+illusionistr@users.noreply.github.com>
2024-05-02 19:20:33 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+sns.set_theme(style="darkgrid")
+
+
+fig = plt.figure(figsize=(10, 5))
+tips = sns.load_dataset("tips")
+sns.relplot(data=tips, x="total_bill", y="tip", hue="smoker")
+st.pyplot()
